63dbe2f37537e56637b1ac0a47352e941425e8c8
ZhangHao <zhanghao@antigen.top>
2025-05-25 23:38:36 +0800

    Fix quotation

--- passes/quote.rkt
+++ passes/quote.rkt
@@ -4,11 +4,19 @@
 
 (define-language L4
   (extends L3)
+  (terminals (+ (constant (c))))
   (Expr (e body)
-        (+ d)))
+        (+ c)))
+
+(define constant?
+  (lambda (v)
+    (or (string? v)
+        (fixnum? v)
+        (flonum? v)
+        (boolean? v))))
 
 (define-parser parse-L4 L4)
 
 (define-pass add-quote : L4 (ir) -> L3 ()
   (Expr : Expr (ir) -> Expr ()
-        (,d `',d)))
+        (,c `',c)))
